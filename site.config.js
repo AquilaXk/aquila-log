8448b0f610d8f80da0ff4099177e9bd94d843174
Aquila <illusiveman7@gmail.com>
2026-02-06 18:25:42 +0900

    Update profile image URL in site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "aquilaXk",
-    image: "https://scontent-icn2-1.cdninstagram.com/v/t51.2885-19/490133522_690190946794350_2967597213309830821_n.jpg?stp=dst-jpg_s320x320_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4zNzUuYzIifQ&_nc_ht=scontent-icn2-1.cdninstagram.com&_nc_cat=106&_nc_oc=Q6cZ2QFnbVOC4pxM7wL3rpF1vUBDWNdOltQp3YrLNG4Xuwf4bUWqpgj00c4e-IsBA23Zx9U&_nc_ohc=UpcYMwV0L0gQ7kNvwEjvol4&_nc_gid=HNDT9rE79-QanvhiVMWLQg&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_Afuc2nAYdIegGeivRIowpsLknPxpi0EZ3mdX9arIDDIXeA&oe=698B82DE&_nc_sid=8b3546", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "https://avatars.githubusercontent.com/u/186246795?s=400&u=b1604e2414a349d30f182809a1061f90a0558a9c&v=4", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "backend developer",
     bio: "I develop everything using node.",
     email: "illusiveman7@gmail.com",
