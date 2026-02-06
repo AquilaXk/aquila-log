415bd8af5301c18683d479667dce636ed7602b14
Aquila <illusiveman7@gmail.com>
2026-02-06 18:18:05 +0900

    Update profile and blog settings in site.config.js

--- site.config.js
+++ site.config.js
@@ -1,32 +1,32 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    name: "aquilaXk",
+    image: "https://scontent-icn2-1.cdninstagram.com/v/t51.2885-19/490133522_690190946794350_2967597213309830821_n.jpg?stp=dst-jpg_s320x320_tt6&efg=eyJ2ZW5jb2RlX3RhZyI6InByb2ZpbGVfcGljLmRqYW5nby4zNzUuYzIifQ&_nc_ht=scontent-icn2-1.cdninstagram.com&_nc_cat=106&_nc_oc=Q6cZ2QFnbVOC4pxM7wL3rpF1vUBDWNdOltQp3YrLNG4Xuwf4bUWqpgj00c4e-IsBA23Zx9U&_nc_ohc=UpcYMwV0L0gQ7kNvwEjvol4&_nc_gid=HNDT9rE79-QanvhiVMWLQg&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_Afuc2nAYdIegGeivRIowpsLknPxpi0EZ3mdX9arIDDIXeA&oe=698B82DE&_nc_sid=8b3546", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "backend developer",
     bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    email: "illusiveman7@gmail.com",
+    linkedin: "",
+    github: "aquilaXk",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `aquilaXk-blog`,
+      href: "https://github.com/aquilaXk/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "aquilaXk's Blog",
+    description: "welcome to my backend dev log!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
   link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  since: 2026, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
@@ -69,7 +69,7 @@ const CONFIG = {
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+  revalidateTime: 3600, // revalidate time for [slug], index
 }
 
 module.exports = { CONFIG }
