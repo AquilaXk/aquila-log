b04302843bec2e963311ab19a543963c909f2980
AquilaXk <illusiveman7@gmail.com>
2026-02-27 22:52:02 +0900

    Load Cusdis dynamically and update deps

--- src/routes/Detail/PostDetail/CommentBox/Cusdis.tsx
+++ src/routes/Detail/PostDetail/CommentBox/Cusdis.tsx
@@ -1,9 +1,11 @@
 import { CONFIG } from "site.config"
-import { ReactCusdis } from "react-cusdis"
-import { useCallback, useEffect, useState } from "react"
+import { useEffect } from "react"
 import styled from "@emotion/styled"
 import useScheme from "src/hooks/useScheme"
-import { useRouter } from "next/router"
+
+type CusdisApi = {
+  initial?: () => void
+}
 
 type Props = {
   id: string
@@ -12,45 +14,45 @@ type Props = {
 }
 
 const Cusdis: React.FC<Props> = ({ id, slug, title }) => {
-  const [value, setValue] = useState(0)
   const [scheme] = useScheme()
 
-  const onDocumentElementChange = useCallback(() => {
-    setValue((value) => value + 1)
-  }, [])
-
   useEffect(() => {
-    const changesObserver = new MutationObserver(
-      (mutations: MutationRecord[]) => {
-        mutations.forEach((mutation: MutationRecord) => {
-          onDocumentElementChange()
-        })
-      }
-    )
+    const thread = document.getElementById("cusdis_thread")
+    if (!thread) return
 
-    changesObserver.observe(document.documentElement, {
-      attributeFilter: ["class"],
-    })
+    const host = CONFIG.cusdis.config.host.replace(/\/$/, "")
 
-    return () => {
-      changesObserver.disconnect()
+    thread.setAttribute("data-host", host)
+    thread.setAttribute("data-app-id", CONFIG.cusdis.config.appid)
+    thread.setAttribute("data-page-id", id)
+    thread.setAttribute("data-page-title", title)
+    thread.setAttribute("data-page-url", `${CONFIG.link}/${slug}`)
+    thread.setAttribute("data-theme", scheme)
+
+    const initialize = () => {
+      const cusdis = (window as Window & { CUSDIS?: CusdisApi }).CUSDIS
+      cusdis?.initial?.()
     }
-  }, [onDocumentElementChange])
+
+    const existingScript = document.getElementById("cusdis_script")
+    if (existingScript) {
+      initialize()
+      return
+    }
+
+    const script = document.createElement("script")
+    script.id = "cusdis_script"
+    script.src = `${host}/js/cusdis.es.js`
+    script.async = true
+    script.defer = true
+    script.onload = initialize
+    document.body.appendChild(script)
+  }, [id, scheme, slug, title])
 
   return (
     <>
       <StyledWrapper id="comments">
-        <ReactCusdis
-          key={value}
-          attrs={{
-            host: CONFIG.cusdis.config.host,
-            appId: CONFIG.cusdis.config.appid,
-            pageId: id,
-            pageTitle: title,
-            pageUrl: `${CONFIG.link}/${slug}`,
-            theme: scheme,
-          }}
-        />
+        <div id="cusdis_thread" />
       </StyledWrapper>
     </>
   )
